85272398ef5636c1ef0b9eaada47c68a21c5d305
whitney smith <whitnaaays@gmail.com>
2020-10-23 16:20:51 -0600

    Added object model and configured group and object viewsets

--- gam_services/urls.py
+++ gam_services/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from operations.views import Group
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('groups/', Group.as_view())
 ]
--- operations/views.py
+++ operations/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import viewsets
+from .models import Group, Object
+from rest_framework.response import Response
+from rest_framework.decorators import action
+
 
 # Create your views here.
+class Group(viewsets.ModelViewset):
+    queryset = Group.objects.all()
+
+
+class Object(viewsets.ModelViewset):
+    queryset = Object.objects.all()
+
+    @action(detail=True, methods=["POST"])
+    def set_filter(self, pk, request):
+        print(pk)
+        print(request)
+        return Response(200)
+
