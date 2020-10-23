74d61cfa474b5b06abf9139641de95bcf5ac55ef
Kenny <kenny@monumetric.com>
2020-10-23 17:04:53 -0600

    updated orders and line items functions so they work

--- operations/tasks.py
+++ operations/tasks.py
@@ -1,4 +1,5 @@
 from .client.dfp import Client
+from googleads import (ad_manager as dfp, oauth2)
 
 
 def get_orders(version='v202008'):
@@ -9,7 +10,7 @@ def get_orders(version='v202008'):
     dfp_account = 20842576
     client = Client(code=dfp_account)
     # order_service = client.GetService('OrderService', version=version)
-    statement = ad_manager.StatementBuilder(version=version)
+    statement = dfp.StatementBuilder(version=version)
     orders = []
     while True:
         response = client.order_service.getOrdersByStatement(statement.ToStatement())
@@ -22,4 +23,25 @@ def get_orders(version='v202008'):
             break
 
     return orders
+
+
+def get_line_items(version='v202008'):
+    dfp_account = 20842576
+    client = Client(code=dfp_account)
+    # client = ad_manager.AdManagerClient.LoadFromStorage()
+    # line_item_service = client.GetService('LineItemService', version='v202008')
+
+    statement = dfp.StatementBuilder(version=version)
+    line_items = []
+    while True:
+        response = client.li_service.getLineItemsByStatement(statement.ToStatement(
+        ))
+        if 'results' in response and len(response['results']):
+            for line_item in response['results']:
+                line_items.append(line_item)
+            statement.offset += statement.limit
+        else:
+            break
+    
+    return line_items
 
