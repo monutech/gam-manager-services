c7b54376033957332593bf77934ef0e37ffc76ab
devin <devin@monumetric.com>
2020-10-23 14:56:56 -0600

    Add static files.

--- gam_services/settings.py
+++ gam_services/settings.py
@@ -131,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "assets")
 
 DFP = {
     'name': os.environ.get('DFP_NAME'),
